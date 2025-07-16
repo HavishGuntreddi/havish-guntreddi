cea239bb79136ae6f042c30312dcca8c272d09c7
Havish Guntreddi <mailtohavish@gmail.com>
2025-07-15 21:56:08 -0400

    photo and responsive adjustment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Havish Guntreddi</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
